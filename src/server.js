e9c0f2d06398384c5ccb5a59c374839ff4cb8e58
pauloalchemist <paulolinsdev@gmail.com>
2020-12-28 23:44:58 -0300

    server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import app from './app.js';
+import app from './app.js'
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-	console.log('Server running at port:', PORT);
-});
+  console.log('Server running at port:', PORT)
+})
