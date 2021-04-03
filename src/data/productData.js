63b6fd2921e09a84c0faf7446ed2d4dd3d756967
pauloalchemist <paulolinsdev@gmail.com>
2021-04-02 23:26:50 -0300

    indation query

--- src/data/productData.js
+++ src/data/productData.js
@@ -2,6 +2,8 @@ const db = require('../infra/connection-db')
 
 module.exports = {
   getProductForExpirationDate () {
-    return db.query('select p_name, lt, date_exp, (date_exp - now()::date) as tempo_validade_em_dias from product.expiration_date;')
+    return db.query(
+      'select p_name, lt, date_exp, (date_exp - now()::date) as tempo_validade_em_dias from product.expiration_date;'
+    )
   }
 }
